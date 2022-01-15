2fe9e600626aadde859974a4df01ec5812a14e9d
melonfacedoom <mmjacobmm@gmail.com>
2022-01-15 05:15:07 +0000

    the little things

--- nitwitch/settings.py
+++ nitwitch/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ebv9v$&!=&be*zp&^xz*ys76q8heb1y26o*9ai5u@zx&mf92j0'
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['68.183.52.128', 'nitwitch.com']
 
 
 # Application definition
